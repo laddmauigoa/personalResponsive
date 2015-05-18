504fcfa701e890c3619d6b0da1794756d5437c3b
ladd mauigoa <laddmauigoa@gmail.com>
2015-05-17 22:26:09 -0600

    server.js fixed

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var port = 8888;
+
 
 var app = express();
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'))
 
 
-app.listen(port, function() {
-	console.log('listening to ' + port)
-});
+app.listen(process.env.EXPRESS_PORT || 8888);
